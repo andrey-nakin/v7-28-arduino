ae127d8ba3b7d818551d3a421e1fb877e41b02d7
Andrey Nakin <andrey.nakin@gmail.com>
2013-10-07 19:46:38 +0400

    modified:   Makefile 	modified:   src/v7-28-arduino.c

--- src/v7-28-arduino.c
+++ src/v7-28-arduino.c
@@ -7,8 +7,10 @@
   Forward declarations
 ******************************************************************************/
 
-static uint16_t supported_modes();
-static bool_t set_mode(uint16_t mode, float range, float resolution);
+static scpimm_mode_t supported_modes();
+static bool_t set_mode(scpimm_mode_t mode);
+static bool_t set_range(scpimm_mode_t mode, const scpi_number_t* range);
+static bool_t get_range(scpimm_mode_t mode, scpi_number_t* range);
 static void trigger();
 static size_t send(const uint8_t* data, size_t len);
 static void set_remote(bool_t remote, bool_t lock);
@@ -20,6 +22,9 @@ static void set_remote(bool_t remote, bool_t lock);
 static scpimm_interface_t scpimm_interface = {
 	supported_modes,
 	set_mode,
+	set_range,
+	get_range,
+	NULL,
 	trigger,
 	send,
 	set_remote
@@ -94,14 +99,18 @@ static uint8_t readDigit(uint8_t pin1, uint8_t pin2, uint8_t pin4, uint8_t pin8)
 	);
 }
 
-static double readNumber() {
+static void readNumber(scpi_number_t * result) {
 	const uint8_t sign = 
 		(digitalRead(PIN_SIGN_4) ? 4 : 0)
 		| (digitalRead(PIN_SIGN_2) ? 2 : 0)
 		| (digitalRead(PIN_SIGN_1) ? 1 : 0);
+
 	if (CODE_SIGN_OVERFLOW == sign) {
-		return SCPIMM_OVERFLOW;
+		result->type = SCPI_NUM_INF;
+		return;
 	}
+
+	result->type = SCPI_NUM_NUMBER;
 
 	double res = 
 		readDigit(PIN_DIGIT1_1, PIN_DIGIT1_2, PIN_DIGIT1_4, PIN_DIGIT1_8)
@@ -129,11 +138,13 @@ static double readNumber() {
 		}
 	}
 
-	return res;
+	result->value = res;
 }
 
 static void valueIsReady() {
-	SCPIMM_acceptValue(readNumber());
+	scpi_number_t num;
+	readNumber(&num);
+	SCPIMM_acceptValue(&num);
 }
 
 static void setRange(const uint8_t bits) {
@@ -148,18 +159,56 @@ static void setAutoRange() {
 	digitalWrite(PIN_AUTO_RANGE, HIGH);
 }
 
+static uint8_t min_range_code(scpimm_mode_t mode) {
+	switch (mode) {
+		case SCPIMM_MODE_DCV:
+		case SCPIMM_MODE_DCV_RATIO:
+		case SCPIMM_MODE_RESISTANCE_2W:
+			return 0x0c;
+
+		case SCPIMM_MODE_ACV:
+			return 0x00;
+	}
+
+	return 0;
+}
+
+static uint8_t max_range_code(scpimm_mode_t mode) {
+	switch (mode) {
+		case SCPIMM_MODE_DCV:
+		case SCPIMM_MODE_DCV_RATIO:
+		case SCPIMM_MODE_ACV:
+			return 0x0a;			
+
+		case SCPIMM_MODE_RESISTANCE_2W:
+			return 0x01;
+			break;			
+	}
+
+	return 0;
+}
+
+static void set_number_range(float r) {
+	setRange(
+		r <= 0.11f ? 0xc0
+		: r <= 1.1f ? 0x00
+		: r <= 11.0f ? 0x80
+		: r <= 110.0f ? 0x02
+		: r <= 1100.0f ? 0x0a
+		: 0x01
+	);
+}
+
 /******************************************************************************
   Multimeter interface implementation
 ******************************************************************************/
 
-static uint16_t supported_modes() {
+static scpimm_mode_t supported_modes() {
 	return SCPIMM_MODE_DCV | SCPIMM_MODE_DCV_RATIO | SCPIMM_MODE_ACV 
 		| SCPIMM_MODE_RESISTANCE_2W;
 }
 
-static bool_t set_mode(const uint16_t mode, float range, float resolution) {
-	uint8_t minRange, maxRange;
-
+static bool_t set_mode(const scpimm_mode_t mode) {
 	switch (mode) {
 		case SCPIMM_MODE_DCV:
 			digitalWrite(PIN_MODE_1, LOW);
@@ -167,10 +216,6 @@ static bool_t set_mode(const uint16_t mode, float range, float resolution) {
 			digitalWrite(PIN_MODE_4, HIGH);
 			digitalWrite(PIN_MODE_8, HIGH);
 			digitalWrite(PIN_MODE_16, LOW);
-
-			minRange = 0x0c;
-			maxRange = 0x0a;			
-
 			break;			
 
 		case SCPIMM_MODE_DCV_RATIO:
@@ -179,10 +224,6 @@ static bool_t set_mode(const uint16_t mode, float range, float resolution) {
 			digitalWrite(PIN_MODE_4, HIGH);
 			digitalWrite(PIN_MODE_8, HIGH);
 			digitalWrite(PIN_MODE_16, HIGH);
-
-			minRange = 0x0c;
-			maxRange = 0x0a;			
-
 			break;			
 
 		case SCPIMM_MODE_ACV:
@@ -191,10 +232,6 @@ static bool_t set_mode(const uint16_t mode, float range, float resolution) {
 			digitalWrite(PIN_MODE_4, HIGH);
 			digitalWrite(PIN_MODE_8, LOW);
 			digitalWrite(PIN_MODE_16, LOW);
-
-			minRange = 0x00;
-			maxRange = 0x0a;			
-
 			break;			
 
 		case SCPIMM_MODE_RESISTANCE_2W:
@@ -203,10 +240,6 @@ static bool_t set_mode(const uint16_t mode, float range, float resolution) {
 			digitalWrite(PIN_MODE_4, LOW);
 			digitalWrite(PIN_MODE_8, HIGH);
 			digitalWrite(PIN_MODE_16, LOW);
-
-			minRange = 0x0c;
-			maxRange = 0x01;
-
 			break;			
 
 		default:
@@ -214,24 +247,70 @@ static bool_t set_mode(const uint16_t mode, float range, float resolution) {
 			return FALSE;
 	}
 
-	if (SCPIMM_RANGE_MIN == range) {
-		setRange(minRange);
-	} else if (SCPIMM_RANGE_MAX == range) {
-		setRange(maxRange);
-	} else if (SCPIMM_RANGE_DEF == range) {
-		setAutoRange();
-	} else {
-		setRange(
-			range <= 0.1 ? 0xc0
-			: range <= 1.0 ? 0x00
-			: range <= 10.0 ? 0x80
-			: range <= 100.0 ? 0x02
-			: range <= 1000.0 ? 0x0a
-			: 0x01
-		);
+	return TRUE;
+}
+
+static bool_t set_range(scpimm_mode_t mode, const scpi_number_t* range) {
+	switch (range->type) {
+		case SCPI_NUM_MIN:
+			setRange(min_range_code(mode));
+			break;
+
+		case SCPI_NUM_MAX:
+			setRange(max_range_code(mode));
+			break;
+
+		case SCPI_NUM_DEF:
+			setAutoRange();
+			break;
+
+		case SCPI_NUM_NUMBER:
+			set_number_range((float) range->value);
+			break;
+
+		default:
+			/* invalid parameters */
+			return FALSE;
 	}
 
 	return TRUE;
+}
+
+static bool_t get_range(scpimm_mode_t mode, scpi_number_t* range) {
+	switch (range->type) {
+		case SCPI_NUM_MIN:
+			switch (mode) {
+				case SCPIMM_MODE_DCV:
+				case SCPIMM_MODE_DCV_RATIO:
+				case SCPIMM_MODE_RESISTANCE_2W:
+					range->value = 0.1;
+					return TRUE;
+				case SCPIMM_MODE_ACV:
+					range->value = 1.0;
+					return TRUE;
+			}
+			break;
+
+		case SCPI_NUM_MAX:
+			switch (mode) {
+				case SCPIMM_MODE_DCV:
+				case SCPIMM_MODE_ACV:
+				case SCPIMM_MODE_DCV_RATIO:
+					range->value = 1000.0;
+					return TRUE;
+
+				case SCPIMM_MODE_RESISTANCE_2W:
+					range->value = 10000.0;
+					return TRUE;
+			}
+			break;
+	
+		default:
+			break;
+	}
+
+	/* invalid parameters */
+	return FALSE;
 }
 
 static void trigger() {

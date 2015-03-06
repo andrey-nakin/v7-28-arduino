28060646dedaf88fbb5557231f11c2a97bcebc5a
Andrey Nakin <andrey.nakin@gmail.com>
2015-03-06 17:26:37 +0300

    modified:   src/consts.h 	modified:   src/v7-28-arduino.c

--- src/v7-28-arduino.c
+++ src/v7-28-arduino.c
@@ -9,14 +9,15 @@
 ******************************************************************************/
 
 static scpimm_mode_t supported_modes();
-static int16_t set_mode(const scpimm_mode_t mode, const scpi_number_t* range, const scpi_number_t* resolution);
-static int16_t get_mode(scpimm_mode_t* mode, scpi_number_t* range, scpi_number_t* resolution);
-static bool_t set_range(scpimm_mode_t mode, const scpi_number_t* range);
-static bool_t get_range(scpimm_mode_t mode, scpi_number_t* range);
+static int16_t set_mode(const scpimm_mode_t mode, const scpimm_mode_params_t* params);
+static int16_t get_mode(scpimm_mode_t* mode, scpimm_mode_params_t* params);
+static int16_t get_possible_range(scpimm_mode_t mode, double* min_range, double* max_range);
+static int16_t get_possible_resolution(scpimm_mode_t  mode, double range, double* min_resolution, double* max_resolution);
 static bool_t start_measure();
 static size_t send(const uint8_t* data, size_t len);
 static void set_remote(bool_t remote, bool_t lock);
 static const char* get_error_description(int16_t error);
+static void set_auto_range(const bool_t enabled);
 
 /******************************************************************************
   Global constants
@@ -26,6 +27,8 @@ static scpimm_interface_t scpimm_interface = {
 	supported_modes,
 	set_mode,
 	get_mode,
+	get_possible_range,
+	get_possible_resolution,
 	start_measure,
 	send,
 	set_remote,
@@ -42,8 +45,8 @@ static scpimm_interface_t scpimm_interface = {
 static struct {
 	uint8_t initialized;
 	scpimm_mode_t mode;
-	scpi_number_t range;
-	scpi_number_t resolution;
+	double range;
+	bool_t auto_range;
 } v7_28_state;
 
 /******************************************************************************
@@ -208,66 +211,17 @@ static void setupPins() {
     attachInterrupt(INT_MEAS_START, valueIsReady, RISING);
     
 	set_disabled(TRUE);
+	set_auto_range(TRUE);	//  enable autorange
 
     digitalWrite(PIN_REMOTE, LOW);  //  enable remote mode
     digitalWrite(PIN_AUTOSTART, LOW);   //  disable autostart
-    digitalWrite(PIN_AUTO_RANGE, HIGH); //  enable autorange
     digitalWrite(PIN_START, LOW);
     
 	set_disabled(FALSE);
 }
 
-static void set_auto_range_disabled(bool_t disabled) {
-	digitalWrite(PIN_AUTO_RANGE, disabled ? LOW : HIGH);
-}
-
-static void setRange(const uint8_t bits) {
-	write_4bits(PIN_RANGE_1,PIN_RANGE_2, PIN_RANGE_4, PIN_RANGE_8, bits); 
-	set_auto_range_disabled(TRUE);
-}
-
-static void setAutoRange() {
-	set_auto_range_disabled(FALSE);
-}
-
-static uint8_t min_range_code(scpimm_mode_t mode) {
-	switch (mode) {
-		case SCPIMM_MODE_DCV:
-		case SCPIMM_MODE_DCV_RATIO:
-		case SCPIMM_MODE_RESISTANCE_2W:
-			return 0x0c;
-
-		case SCPIMM_MODE_ACV:
-			return 0x00;
-	}
-
-	return 0;
-}
-
-static uint8_t max_range_code(scpimm_mode_t mode) {
-	switch (mode) {
-		case SCPIMM_MODE_DCV:
-		case SCPIMM_MODE_DCV_RATIO:
-		case SCPIMM_MODE_ACV:
-			return 0x0a;			
-
-		case SCPIMM_MODE_RESISTANCE_2W:
-			return 0x01;
-			break;			
-	}
-
-	return 0;
-}
-
-static void set_number_range(float r) {
-	setRange(
-		r <= 0.11f ? 0xc0
-		: r <= 1.1f ? 0x00
-		: r <= 11.0f ? 0x80
-		: r <= 110.0f ? 0x02
-		: r <= 1100.0f ? 0x0a
-		: 0x01
-	);
+static void set_auto_range(const bool_t enabled) {
+	digitalWrite(PIN_AUTO_RANGE, enabled ? HIGH : LOW);
 }
 
 static bool_t is_state_initialized(uint8_t bits) {
@@ -282,77 +236,78 @@ static bool_t is_mode_initialized(void) {
 	return is_state_initialized(V7_28_STATE_INITIALIZED_MODE);
 }
 
-static void set_range_impl(scpimm_mode_t mode, const scpi_number_t* range) {
-	// TODO: question 28
-	switch (range->type) {
-		case SCPI_NUM_MIN:
-			setRange(min_range_code(mode));
-			break;
-
-		case SCPI_NUM_MAX:
-			setRange(max_range_code(mode));
-			break;
-
-		case SCPI_NUM_DEF:
-			setAutoRange();
-			break;
-
-		case SCPI_NUM_NUMBER:
-			set_number_range((float) range->value);
-			break;
+static uint8_t get_dcv_range_code(double range) {
+	if (range <= 1.0e-1) {
+		return V7_28_RANGE_DCV_0;
 	}
+	if (range <= 1.0e0) {
+		return V7_28_RANGE_DCV_1;
+	}
+	if (range <= 1.0e1) {
+		return V7_28_RANGE_DCV_2;
+	}
+	if (range <= 1.0e2) {
+		return V7_28_RANGE_DCV_3;
+	}
+	return V7_28_RANGE_DCV_4;
 }
 
-static int16_t set_mode_impl(const scpimm_mode_t mode, const scpi_number_t* range, const scpi_number_t* resolution) {
-	uint8_t mode_code, expected;
-	int n = V7_28_SET_MODE_MAX_STEPS;
+static uint8_t get_acv_range_code(double range) {
+	if (range <= 1.0e0) {
+		return V7_28_RANGE_ACV_0;
+	}
+	if (range <= 1.0e1) {
+		return V7_28_RANGE_ACV_1;
+	}
+	if (range <= 1.0e2) {
+		return V7_28_RANGE_ACV_2;
+	}
+	return V7_28_RANGE_ACV_3;
+}
+
+static uint8_t get_resistance_range_code(double range) {
+	if (range <= 1.0e-1) {
+		return V7_28_RANGE_RESISTANCE_0;
+	}
+	if (range <= 1.0e0) {
+		return V7_28_RANGE_RESISTANCE_1;
+	}
+	if (range <= 1.0e1) {
+		return V7_28_RANGE_RESISTANCE_2;
+	}
+	if (range <= 1.0e2) {
+		return V7_28_RANGE_RESISTANCE_3;
+	}
+	if (range <= 1.0e3) {
+		return V7_28_RANGE_RESISTANCE_4;
+	}
+	return V7_28_RANGE_RESISTANCE_5;
+}
+
+static void set_range(const scpimm_mode_t mode, double range) {
+	uint8_t code;
+
+	range *= 0.99;
 
 	switch (mode) {
-		case SCPIMM_MODE_DCV:
-			mode_code = V7_28_MODE_DCV;
-			expected = V7_28_READ_MODE_DCV;
-			break;			
+	case SCPIMM_MODE_DCV:
+	case SCPIMM_MODE_DCV_RATIO:
+		code = get_dcv_range_code(range);
+		break;
 
-		case SCPIMM_MODE_DCV_RATIO:
-			mode_code = V7_28_MODE_DCV_RATIO;
-			expected = V7_28_READ_MODE_DCV_RATIO;
-			break;			
+	case SCPIMM_MODE_ACV:
+		code = get_acv_range_code(range);
+		break;
 
-		case SCPIMM_MODE_ACV:
-			mode_code = V7_28_MODE_ACV;
-			expected = V7_28_READ_MODE_ACV;
-			break;			
+	case SCPIMM_MODE_RESISTANCE_2W:
+		code = get_resistance_range_code(range);
+		break;
 
-		case SCPIMM_MODE_RESISTANCE_2W:
-			mode_code = V7_28_MODE_RESISTANCE_2W;
-			expected = V7_28_READ_MODE_RESISTANCE_2W;
-			break;			
-
-		default:
-			// mode is not supported
-			return V7_28_ERROR_INVALID_MODE;
+	default:
+		return;
 	}
 
-    set_disabled(TRUE);
-	write_5bits(PIN_MODE_1, PIN_MODE_2, PIN_MODE_4, PIN_MODE_8, PIN_MODE_16, mode_code);
-	if (range) {
-		set_range_impl(mode, range);
-	}
-    set_disabled(FALSE);
-
-	while (n--) {
-		const uint8_t read_mode = read_4bits(PIN_READ_MODE_1, PIN_READ_MODE_2, PIN_READ_MODE_4, PIN_READ_MODE_8);
-		if (read_mode == expected) {
-			v7_28_state.mode = mode;
-			set_state_initialized(V7_28_STATE_INITIALIZED_MODE);
-			return SCPI_ERROR_OK;
-		}
-
-		delay(V7_28_SET_MODE_DELAY);
-	}
-
-	// multimeter could not set to given mode
-	return V7_28_ERROR_SET_MODE;
+	write_4bits(PIN_RANGE_1, PIN_RANGE_2, PIN_RANGE_4, PIN_RANGE_8, code);
 }
 
 /******************************************************************************
@@ -360,30 +315,91 @@ static int16_t set_mode_impl(const scpimm_mode_t mode, const scpi_number_t* rang
 ******************************************************************************/
 
 static scpimm_mode_t supported_modes() {
-	return SCPIMM_MODE_DCV | SCPIMM_MODE_DCV_RATIO | SCPIMM_MODE_ACV 
+	return SCPIMM_MODE_DCV | SCPIMM_MODE_DCV_RATIO | SCPIMM_MODE_ACV
 		| SCPIMM_MODE_RESISTANCE_2W;
 }
 
-static int16_t set_mode(const scpimm_mode_t mode, const scpi_number_t* range, const scpi_number_t* resolution) {
-	if (!is_mode_initialized() || v7_28_state.mode != mode) {
-		// current mode is different or not specified, change it to given one
-		return set_mode_impl(mode, range, resolution);
-	} else {
-		// keep current mode intact
+static int16_t set_mode(const scpimm_mode_t mode, const scpimm_mode_params_t* params) {
+	bool_t change_mode = TRUE;
+	uint8_t mode_code, expected;
+	int n = V7_28_SET_MODE_MAX_STEPS;
 
-		if (range) {
-			// set range
-			set_disabled(TRUE);
-			set_range_impl(mode, range);
-			set_disabled(FALSE);
+	if (is_mode_initialized()) {
+		change_mode = mode != v7_28_state.mode;
+	}
+
+	if (change_mode) {
+		switch (mode) {
+			case SCPIMM_MODE_DCV:
+				mode_code = V7_28_MODE_DCV;
+				expected = V7_28_READ_MODE_DCV;
+				break;
+
+			case SCPIMM_MODE_DCV_RATIO:
+				mode_code = V7_28_MODE_DCV_RATIO;
+				expected = V7_28_READ_MODE_DCV_RATIO;
+				break;
+
+			case SCPIMM_MODE_ACV:
+				mode_code = V7_28_MODE_ACV;
+				expected = V7_28_READ_MODE_ACV;
+				break;
+
+			case SCPIMM_MODE_RESISTANCE_2W:
+				mode_code = V7_28_MODE_RESISTANCE_2W;
+				expected = V7_28_READ_MODE_RESISTANCE_2W;
+				break;
+
+			default:
+				// mode is not supported
+				return V7_28_ERROR_INVALID_MODE;
+		}
+	}
+
+	// disable remote control before we change state of control pins
+    set_disabled(TRUE);
+
+    if (change_mode) {
+    	write_5bits(PIN_MODE_1, PIN_MODE_2, PIN_MODE_4, PIN_MODE_8, PIN_MODE_16, mode_code);
+    }
+
+	if (params) {
+		set_range(mode, params->range);
+		set_state_initialized(V7_28_STATE_INITIALIZED_RANGE);
+		v7_28_state.range = params->range;
+
+		set_auto_range(params->auto_range);
+		set_state_initialized(V7_28_STATE_INITIALIZED_AUTO_RANGE);
+		v7_28_state.auto_range = params->auto_range;
+
+		// params->resolution is ignored
+	}
+
+	// enable remote control for changes to take an effect
+    set_disabled(FALSE);
+
+    if (change_mode) {
+    	// wait until multimeter actually switches to given mode
+		while (n--) {
+			const uint8_t read_mode = read_4bits(PIN_READ_MODE_1, PIN_READ_MODE_2, PIN_READ_MODE_4, PIN_READ_MODE_8);
+			if (read_mode == expected) {
+				v7_28_state.mode = mode;
+				set_state_initialized(V7_28_STATE_INITIALIZED_MODE);
+				return SCPI_ERROR_OK;
+			}
+
+			delay(V7_28_SET_MODE_DELAY);
 		}
 
-		// TODO check resolution
-		return SCPI_ERROR_OK;
-	}
+		// multimeter could not switch to given mode
+		return V7_28_ERROR_SET_MODE;
+    } else {
+    	// no need to wait
+    	return SCPI_ERROR_OK;
+    }
 }
 
-static int16_t get_mode(scpimm_mode_t* mode, scpi_number_t* range, scpi_number_t* resolution) {
+static int16_t get_mode(scpimm_mode_t* mode, scpimm_mode_params_t* params) {
 	if (mode) {
 		if (!is_mode_initialized()) {
 			return V7_28_ERROR_MODE_NOT_INITIALIZED;
@@ -391,59 +407,70 @@ static int16_t get_mode(scpimm_mode_t* mode, scpi_number_t* range, scpi_number_t
 		*mode = v7_28_state.mode;
 	}
 
-	if (range) {
-		if (!is_state_initialized(V7_28_STATE_INITIALIZED_RANGE)) {
-			return V7_28_ERROR_RANGE_NOT_INITIALIZED;
+	if (params) {
+		if (!is_state_initialized(V7_28_STATE_INITIALIZED_AUTO_RANGE)) {
+			return V7_28_ERROR_AUTO_RANGE_NOT_INITIALIZED;
 		}
-		*range = v7_28_state.range;
-	}
+		params->auto_range = v7_28_state.auto_range;
 
-	if (resolution) {
-		if (!is_state_initialized(V7_28_STATE_INITIALIZED_RESOLUTION)) {
-			return V7_28_ERROR_RESOLUTION_NOT_INITIALIZED;
-		}
-		*resolution = v7_28_state.resolution;
+		// TODO
 	}
 
 	return SCPI_ERROR_OK;
 }
 
-/* TODO deprecated */
-static bool_t get_range(scpimm_mode_t mode, scpi_number_t* range) {
-	switch (range->type) {
-		case SCPI_NUM_MIN:
-			switch (mode) {
-				case SCPIMM_MODE_DCV:
-				case SCPIMM_MODE_DCV_RATIO:
-				case SCPIMM_MODE_RESISTANCE_2W:
-					range->value = 0.1;
-					return TRUE;
-				case SCPIMM_MODE_ACV:
-					range->value = 1.0;
-					return TRUE;
-			}
-			break;
+static int16_t get_possible_range(const scpimm_mode_t mode, double* const min_range, double* const max_range) {
+	double min_value, max_value;
 
-		case SCPI_NUM_MAX:
-			switch (mode) {
-				case SCPIMM_MODE_DCV:
-				case SCPIMM_MODE_ACV:
-				case SCPIMM_MODE_DCV_RATIO:
-					range->value = 1000.0;
-					return TRUE;
+	switch (mode) {
+	case SCPIMM_MODE_DCV:
+	case SCPIMM_MODE_DCV_RATIO:
+		min_value = 0.1;
+		max_value = 1000.0;
+		break;
 
-				case SCPIMM_MODE_RESISTANCE_2W:
-					range->value = 10000.0;
-					return TRUE;
-			}
-			break;
-	
-		default:
-			break;
+	case SCPIMM_MODE_ACV:
+		min_value = 1.0;
+		max_value = 1000.0;
+		break;
+
+	case SCPIMM_MODE_RESISTANCE_2W:
+		min_value = 0.1;
+		max_value = 10000.0;
+		break;
+
+	default:
+		return SCPI_ERROR_ILLEGAL_PARAMETER_VALUE;
 	}
 
-	/* invalid parameters */
-	return FALSE;
+	if (min_range) {
+		*min_range = min_value;
+	}
+
+	if (max_range) {
+		*max_range = max_value;
+	}
+
+	return SCPI_ERROR_OK;
+}
+
+static int16_t get_possible_resolution(const scpimm_mode_t /* mode */, const double range, double* const min_resolution, double* const max_resolution) {
+
+	/*
+	 * always return the same resolution which is 1.0e-6 of given range
+	 * actual resolution is 10 times worse, we're just trying emulate HP 34401A
+	 */
+	const double resolution = range * 1.0e-6;
+
+	if (min_resolution) {
+		*min_resolution = resolution;
+	}
+
+	if (max_resolution) {
+		*max_resolution = resolution;
+	}
+
+	return SCPI_ERROR_OK;
 }
 
 static bool_t start_measure() {
@@ -480,7 +507,6 @@ static const char* get_error_description(int16_t error) {
 void setup() {
 	Serial.begin(BAUD_RATE);
 	setupPins();
-	set_mode(SCPIMM_MODE_DCV, NULL, NULL);
 
 	SCPIMM_setup(&scpimm_interface);
 }
